a301b889a1281ec7ff3d7fce2fdd44a46c1eb5c7
LeCercleCrypto <lecerclecrypto.help@gmail.com>
2024-10-04 19:20:49 +0200

    v1.1.1

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-let score = 0;
-
-const scoreDisplay = document.getElementById('score');
-const tapButton = document.getElementById('tapButton');
-
-tapButton.addEventListener('click', () => {
-    score++;
-    scoreDisplay.textContent = `Points: ${score}`;
-});
 
 document.addEventListener('dblclick', function (event) {
-    event.preventDefault(); // Empêche l'action par défaut du double-clic
+    event.preventDefault();
 });
 
 function redirect(opt){
