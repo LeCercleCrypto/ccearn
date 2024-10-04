002bc7279ef7d3fb7ad636ece0467dc9d3ef565b
LeCercleCrypto <lecerclecrypto.help@gmail.com>
2024-10-04 18:44:22 +0200

    v1.1.1

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ tapButton.addEventListener('click', () => {
 document.addEventListener('dblclick', function (event) {
     event.preventDefault(); // Empêche l'action par défaut du double-clic
 });
+
+function redirect(opt){
+    switch(opt){
+        case 'invest':
+            document.location.href = 'invest.html';
+            break;
+        case 'earn':
+            document.location.href = 'index.html';
+            break;
+        case 'business':
+            document.location.href = 'business.html';
+            break;
+        case 'profile':
+            document.location.href = 'profile.html';
+            break;
+        case 'company':
+            document.location.href = 'company.html';
+            break;
+            
+    }
+}
